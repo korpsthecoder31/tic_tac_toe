18ff910cefb62a4f691f55224e0c7ad211c2bd85
korpsthecoder31 <korpsthewriter@gmail.com>
2025-02-11 19:22:51 +0800

    Create replay_tic_tac_toe_method
    	- Prompt user whether play again
    	- if yes, play again
    	- if no, print goodbye message and exit
    	- Ensure replay is recursive

--- main.rb
+++ main.rb
@@ -6,6 +6,20 @@ def run_tic_tac_toe
   game = Game.new
   game.intro
   game.play_game
+  replay_tic_tac_toe(game)
+end
+
+def replay_tic_tac_toe(game)
+  puts "Would you like to play again? [Enter 'Y' to play again]"
+  print ": "
+  play_again = gets.chomp.upcase
+  if play_again == "Y"
+    game.clear_board
+    game.play_game
+    replay_tic_tac_toe(game)
+  else
+    puts "Thanks for playing. Goodbye."
+  end
 end 
 
 run_tic_tac_toe
